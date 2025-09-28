066de21e23012e8b6d8f3dc9d41ec0d1fff0421a
neizvestniy <mirzamidinovziyobek@gmail.com>
2025-09-28 13:40:10 +0500

    finish yhe task

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
+# 1-mmisol
 # SELECT *
 # FROM customers
-#
+#2-misol
 # SELECT contact_name, city
 # FROM customers
-#
+#3-misol
 # SELECT order_id, shipped_date, order_date
 # FROM orders
 #
-#
+#4-misol
 # SELECT city,country
 # FROM customers
 #
-#
+#5-misol
 # SELECT COUNT (*)
 # FROM customers
 #
-#
+#6-misol
 # SELECT COUNT(country)
 # FROM customers
